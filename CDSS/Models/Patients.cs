c445bfbc33522249b5cf7df0d166d753383b4980
YAP EN QI <21022314@myrp.edu.sg>
2023-11-24 21:19:55 +0800

    changes in layout - enqi

--- CDSS/Models/Patients.cs
+++ CDSS/Models/Patients.cs
@@ -6,11 +6,14 @@ namespace CDSS.Models;
 public partial class Patients
 {
     public int PatientId { get; set; }
-
+    [Required(ErrorMessage = "Please enter the first name.")]
     public string FirstName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Please enter the last name.")]
     public string LastName { get; set; } = null!;
 
+    [Required(ErrorMessage = "Please enter the birthdate.")]
+    [DataType(DataType.Date)]
     public DateTime? Birthdate { get; set; }
 
     public string FullName => $"{FirstName} {LastName}";
@@ -19,15 +22,19 @@ public partial class Patients
     {
         Birthdate = DateTime.MinValue.Date;
     }
-
+    [Required(ErrorMessage = "Please enter the ward.")]
     public string? Ward { get; set; }
 
+    [Required(ErrorMessage = "Please enter the bed.")]
     public string? Bed { get; set; }
 
+    [Required(ErrorMessage = "Please enter the weight.")]
     public decimal? Weight { get; set; }
 
+    [Required(ErrorMessage = "Please enter the blood type.")]
     public string? BloodType { get; set; }
 
+    [Required(ErrorMessage = "Please select at least one medical condition.")]
     public string? MedicalCondition { get; set; }
 
     public virtual ICollection<Appointments> Appointments { get; set; } = new List<Appointments>();
