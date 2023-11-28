80a9470a0c2457c3b4275ce7541ea33f8241cd86
YAP EN QI <21022314@myrp.edu.sg>
2023-11-28 18:42:53 +0800

    Patient code updated

--- CDSS/Controllers/PatientsController.cs
+++ CDSS/Controllers/PatientsController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CDSS.Models;
 using CDSS.Services;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 
 namespace CDSS.Controllers
 {
@@ -22,9 +24,14 @@ namespace CDSS.Controllers
         // GET: Patients
         public async Task<IActionResult> Index()
         {
-            return _context.Patients != null ?
-                        View(await _context.Patients.ToListAsync()) :
-                        Problem("Entity set 'AppDbContext.Patients'  is null.");
+            if (_context != null && _context.Patients != null)
+            {
+                return View(await _context.Patients.ToListAsync());
+            }
+            else
+            {
+                return Problem("AppDbContext or its entity set 'Patients' is null.");
+            }
         }
 
         // GET: Patients/Details/5
@@ -53,54 +60,128 @@ namespace CDSS.Controllers
             {
                 return NotFound();
             }
+            // Log or display validation errors
+            foreach (var modelState in ModelState.Values)
+            {
+                if (modelState.ValidationState == ModelValidationState.Invalid)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        Console.WriteLine(error.ErrorMessage);
+                    }
+                }
+            }
+
+
 
             return View(patient);
         }
-
         // GET: Patients/Create
         public IActionResult Create()
         {
+            // Get the distinct medical conditions from the existing data
+            var allMedicalConditions = _context.Patients
+                 .Where(p => !string.IsNullOrEmpty(p.MedicalCondition))
+                 .Select(p => (p.MedicalCondition ?? "").Split(',', StringSplitOptions.RemoveEmptyEntries))
+                 .AsEnumerable() // Materialize the query on the server side
+                 .SelectMany(splitConditions => splitConditions)
+                 .Select(trimmedCondition => trimmedCondition.Trim())
+                 .Where(trimmedCondition => !string.IsNullOrEmpty(trimmedCondition))
+                 .Distinct()
+                 .ToArray();
+
+            ViewBag.AllMedicalConditions = allMedicalConditions;
+
+
             return View();
         }
+
+
 
         // POST: Patients/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("PatientId, FirstName, LastName, Birthdate, Ward, Bed, Weight, BloodType, MedicalCondition")] Patients patients)
+        public async Task<IActionResult> Create([Bind("PatientId, FirstName, LastName, Birthdate, Ward, Bed, Weight, BloodType, MedicalCondition")] Patients patients, string[] MedicalCondition)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(patients);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    // Set the MedicalCondition property using the array
+                    patients.MedicalCondition = string.Join(",", MedicalCondition);
+
+                    _context.Add(patients);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception
+                    Console.WriteLine(ex.Message);
+                    // Handle the exception appropriately
+                    return View("Error");
+                }
             }
+
+            // Repopulate ViewBag.MedicalConditions in case of validation errors
+            ViewBag.MedicalConditions = _context.Patients
+                .Select(p => p.MedicalCondition)
+                .ToList();
+
             return View(patients);
         }
+
+
+
 
         // GET: Patients/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public IActionResult Edit(int? id)
         {
-            if (id == null || _context.Patients == null)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            var patients = await _context.Patients.FindAsync(id);
-            if (patients == null)
+            var patient = _context.Patients.Find(id);
+
+            if (patient == null)
             {
                 return NotFound();
             }
-            return View(patients);
+
+            // Get all distinct medical conditions from the existing data
+            var allMedicalConditions = _context.Patients
+                .Where(p => !string.IsNullOrEmpty(p.MedicalCondition))
+                .Select(p => (p.MedicalCondition ?? "").Split(',', StringSplitOptions.RemoveEmptyEntries))
+                .AsEnumerable() // Materialize the query on the server side
+                .SelectMany(splitConditions => splitConditions)
+                .Select(trimmedCondition => trimmedCondition.Trim())
+                .Where(trimmedCondition => !string.IsNullOrEmpty(trimmedCondition))
+                .Distinct()
+                .ToArray();
+
+            ViewBag.AllMedicalConditions = allMedicalConditions;
+
+            // Split the MedicalCondition of the patient into an array for pre-selection
+            var selectedConditions = (patient.MedicalCondition ?? "").Split(',', StringSplitOptions.RemoveEmptyEntries)?.Select(c => c.Trim()) ?? Array.Empty<string>();
+
+            // Pass the selected conditions as an array to the view
+            ViewBag.SelectedMedicalConditions = selectedConditions;
+         
+
+            return View(patient);
         }
+
+
 
         // POST: Patients/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("PatientId,FirstName,LastName,Birthdate,Ward,Bed,Weight,BloodType,MedicalCondition")] Patients patients)
+        public async Task<IActionResult> Edit(int id, [Bind("PatientId, FirstName, LastName, Birthdate, Ward, Bed, Weight, BloodType, MedicalCondition")] Patients patients, string[] MedicalCondition)
         {
             if (id != patients.PatientId)
             {
@@ -111,24 +192,36 @@ namespace CDSS.Controllers
             {
                 try
                 {
+                    // Ensure unique medical conditions in the array
+                    var uniqueMedicalConditions = MedicalCondition?.Distinct().ToArray() ?? Array.Empty<string>();
+
+                    // Set the MedicalCondition property using the array
+                    patients.MedicalCondition = string.Join(",", uniqueMedicalConditions);
+
                     _context.Update(patients);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (Exception ex)
                 {
-                    if (!PatientsExists(patients.PatientId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    // Log the exception
+                    Console.WriteLine(ex.Message);
+                    // Handle the exception appropriately
+                    return View("Error");
                 }
-                return RedirectToAction(nameof(Index));
             }
+
+            // Repopulate ViewBag.MedicalConditions in case of validation errors
+            ViewBag.MedicalConditions = _context.Patients
+                .Select(p => p.MedicalCondition)
+                .ToList();
+
+            ViewBag.SelectedMedicalConditions = MedicalCondition; // Pass selected conditions back to the view
+
             return View(patients);
         }
+
+
 
         // GET: Patients/Delete/5
         public async Task<IActionResult> Delete(int? id)
